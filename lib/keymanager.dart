1870a9e72e06d7fbb3ce68b9a1151f4debf3c02d
Micky Socaci <micky@nowlive.ro>
2020-03-02 20:29:32 +0200

    Update to proper ethereum addresses, now uses async init

--- lib/keymanager.dart
+++ lib/keymanager.dart
@@ -5,9 +5,12 @@ import 'dart:typed_data';
 import 'package:convert/convert.dart';
 import 'package:bip32/bip32.dart' as bip32;
 import 'package:bip39/bip39.dart' as bip39;
+import 'package:web3dart/web3dart.dart';
+import 'package:web3dart/crypto.dart';
 
 class KeyManager {
   bip32.BIP32 rootkey;
+  String _mnemonic;
 
   String _derrivationPathWallet = 'm/44\'/60\'/0\'/0';
   int _derrivationChildWallet = 0;
@@ -18,32 +21,52 @@ class KeyManager {
 
   HashMap<String,String> _keys = new HashMap();
 
-  KeyManager(String mnemonic) {
-    init(bip39.mnemonicToSeed(mnemonic));
+  KeyManager(String _mnemonic) {
+    this._mnemonic = _mnemonic;
   }
 
-  KeyManager.fromMnemonic(String mnemonic) {
-    init(bip39.mnemonicToSeed(mnemonic));
+  KeyManager.fromMnemonic(String _mnemonic) {
+    this._mnemonic = _mnemonic;
   }
 
-  void init(Uint8List _seed) {
-    this.rootkey = bip32.BIP32.fromSeed(_seed);
+  Future<void> init() async {
+    this.rootkey = bip32.BIP32.fromSeed(bip39.mnemonicToSeed(this._mnemonic));
     
     bip32.BIP32 keyWallet = this.rootkey.derivePath(this._derrivationPathWallet).derive(this._derrivationChildWallet);
     bip32.BIP32 keyStorage = this.rootkey.derivePath(this._derrivationPathStorage).derive(this._derrivationChildStorage);
     bip32.BIP32 keyComms = this.rootkey.derivePath(this._derrivationPathCommunication).derive(this._derrivationChildCommunication);
 
-    this._keys["wallet_privateKey"] = hex.encode(keyWallet.privateKey);
-    this._keys["wallet_publicKey"] = hex.encode(keyWallet.publicKey);
-    this._keys["storage_privateKey"] = hex.encode(keyStorage.privateKey);
-    this._keys["storage_publicKey"] = hex.encode(keyStorage.publicKey);
-    this._keys["comms_privateKey"] = hex.encode(keyComms.privateKey);
-    this._keys["comms_publicKey"] = hex.encode(keyComms.publicKey);
+    this._keys["wallet_privateKey"] = this._getPrivate(keyWallet);
+    this._keys["wallet_publicKey"] = this._privateToPublic(keyWallet);
+    String address = await this._publicKeyToAddress(keyWallet);
+    this._keys["wallet_address"] = address;
+
+    this._keys["storage_privateKey"] = this._getPrivate(keyStorage);
+    this._keys["storage_publicKey"] = this._privateToPublic(keyStorage);
+    this._keys["comms_privateKey"] = this._getPrivate(keyComms);
+    this._keys["comms_publicKey"] =this._privateToPublic(keyComms);
+
   }
 
+  String _getPrivate(dynamic _key) {
+    return hex.encode(_key.privateKey);
+  }
+
+  String _privateToPublic(dynamic _key) {
+    return hex.encode(privateKeyBytesToPublic(_key.privateKey));
+  }
+
+  Future<String> _publicKeyToAddress(dynamic _key) async {
+    Credentials privateKey = EthPrivateKey.fromHex( this._getPrivate(_key) );
+    var address = await privateKey.extractAddress();
+    // privateKey.
+    // privateKeyBytesToPublic(hex.decode(key));
+
+    return address.toString();
+  }
+  
   HashMap<String,String> get keys {
     return this._keys;
   }
-
 
 }
--- test/keymanager_test.dart
+++ test/keymanager_test.dart
@@ -1,14 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:keymanager/keymanager.dart';
 
-void main() {
+void main() async {
 
   String mnemonic = "exchange neither monster ethics bless cancel ghost excite business record warfare invite";
-  String ethereumWallet0PrivateKey = "e49c840fcb71fafcaa068c7d45a6b99f8d5b6064effe7d793b6490641e75cca8";
+  String ethereumWallet0privateKey = "e49c840fcb71fafcaa068c7d45a6b99f8d5b6064effe7d793b6490641e75cca8";
+  String ethereumWallet0publicKey = "30bfa6298178e3ab1f4a2e5d5c3c7d79505c0b3ef7958ac0fec319a67d3e47eb01f05a7059311a6d061b7a4c1eff38549909b0782256e22f748cb9e6f3c4c4a4";
+  String ethereumWallet0address = "0x9d9216e0a29468be1ecacc351ce3887be8a26222";
+
   final myKeyManager = new KeyManager(mnemonic);
+  await myKeyManager.init();
 
   test('Generates correct wallet 0 private key', () {
-    expect(myKeyManager.keys["wallet_privateKey"], ethereumWallet0PrivateKey);
+    expect(myKeyManager.keys["wallet_privateKey"], ethereumWallet0privateKey);
+  });
+
+  test('Generates correct wallet 0 public key', () {
+    expect(myKeyManager.keys["wallet_publicKey"], ethereumWallet0publicKey);
+  });
+
+  test('Generates correct wallet 0 address', () {
+    expect(myKeyManager.keys["wallet_address"], ethereumWallet0address);
   });
 
   test('Generates all 6 keys', () {
@@ -16,9 +28,19 @@ void main() {
     expect(myKeyManager.keys["storage_privateKey"].length, 64);
     expect(myKeyManager.keys["comms_privateKey"].length, 64);
 
-    expect(myKeyManager.keys["wallet_publicKey"].length, 66);
-    expect(myKeyManager.keys["storage_publicKey"].length, 66);
-    expect(myKeyManager.keys["comms_publicKey"].length, 66);
+    expect(myKeyManager.keys["wallet_publicKey"].length, 128);
+    expect(myKeyManager.keys["storage_publicKey"].length, 128);
+    expect(myKeyManager.keys["comms_publicKey"].length, 128);
   });
 
+  // debug
+  print("wallet_privateKey:  " + myKeyManager.keys["wallet_privateKey"]);
+  print("wallet_publicKey:   " + myKeyManager.keys["wallet_publicKey"]);
+  print("wallet_address:     " + myKeyManager.keys["wallet_address"]);
+  print("storage_privateKey: " + myKeyManager.keys["storage_privateKey"]);
+  print("storage_publicKey:  " + myKeyManager.keys["storage_publicKey"]);
+  print("comms_privateKey:   " + myKeyManager.keys["comms_privateKey"]);
+  print("comms_publicKey:    " + myKeyManager.keys["comms_publicKey"]);
+
+  
 }
