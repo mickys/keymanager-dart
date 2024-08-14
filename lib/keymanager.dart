3783558108dc88fea551bb4f7e124c10a5961698
Micky Socaci <micky@nowlive.ro>
2024-08-14 21:07:57 +0300

    Upgraded to dart / flutter 3, replaced ethereum_util with blockchain_utils

--- lib/keymanager.dart
+++ lib/keymanager.dart
@@ -2,95 +2,124 @@ library keymanager;
 
 import 'dart:collection';
 import 'dart:typed_data';
-import 'package:convert/convert.dart';
 import 'package:bip32/bip32.dart' as bip32;
 import 'package:bip39/bip39.dart' as bip39;
-import 'package:ethereum_util/ethereum_util.dart';
+
+// import 'package:blockchain_utils/bip/address/eth_addr.dart';
+import 'package:blockchain_utils/blockchain_utils.dart';
+import 'package:blockchain_utils/crypto/crypto/cdsa/point/base.dart';
 
 
 class KeyManager {
   late bip32.BIP32 rootkey;
   late String _mnemonic;
 
-  String _derrivationPathWallet = 'm/44\'/60\'/0\'/0';
-  int _derrivationChildWallet = 0;
-  String _derrivationPathStorage = 'm/0\'/60\'/0\'/0';
+  final String _derrivationPathWallet = 'm/44\'/60\'/0\'/0';
+  final int _derrivationChildWallet = 0;
+  final String _derrivationPathStorage = 'm/0\'/60\'/0\'/0';
   int _derrivationChildStorage = 0;
-  String _derrivationPathCommunication = 'm/1\'/60\'/0\'/0';
-  int _derrivationChildCommunication = 0;
+  final String _derrivationPathCommunication = 'm/1\'/60\'/0\'/0';
+  final int _derrivationChildCommunication = 0;
 
-  HashMap<String,String> _keys = new HashMap();
+  final HashMap<String,String> _keys = HashMap();
 
-  KeyManager(String _mnemonic) {
-    this._mnemonic = _mnemonic;
+  KeyManager(String mnemonic) {
+    _mnemonic = mnemonic;
   }
 
-  KeyManager.fromMnemonic(String _mnemonic) {
-    this._mnemonic = _mnemonic;
+  KeyManager.fromMnemonic(String mnemonic) {
+    _mnemonic = mnemonic;
   }
 
   Future<void> init() async {
 
-    this.rootkey = bip32.BIP32.fromSeed(bip39.mnemonicToSeed(this._mnemonic));
+    rootkey = bip32.BIP32.fromSeed(bip39.mnemonicToSeed(_mnemonic));
 
-    Uint8List seed = bip39.mnemonicToSeed(this._mnemonic);
-    this.rootkey = bip32.BIP32.fromSeed(seed);
+    Uint8List seed = bip39.mnemonicToSeed(_mnemonic);
+    rootkey = bip32.BIP32.fromSeed(seed);
 
-    bip32.BIP32 keyWallet = this.rootkey.derivePath(this._derrivationPathWallet).derive(this._derrivationChildWallet);
-    bip32.BIP32 keyStorage = this.rootkey.derivePath(this._derrivationPathStorage).derive(this._derrivationChildStorage);
-    bip32.BIP32 keyStorageVdb =  this.rootkey.derivePath(this._derrivationPathStorage).derive(++this._derrivationChildStorage);
-    bip32.BIP32 keyStorageVdc =  this.rootkey.derivePath(this._derrivationPathStorage).derive(++this._derrivationChildStorage);
-    bip32.BIP32 keyComms = this.rootkey.derivePath(this._derrivationPathCommunication).derive(this._derrivationChildCommunication);
+    bip32.BIP32 keyWallet = rootkey.derivePath(_derrivationPathWallet).derive(_derrivationChildWallet);
+    bip32.BIP32 keyStorage = rootkey.derivePath(_derrivationPathStorage).derive(_derrivationChildStorage);
+    bip32.BIP32 keyStorageVdb =  rootkey.derivePath(_derrivationPathStorage).derive(++_derrivationChildStorage);
+    bip32.BIP32 keyStorageVdc =  rootkey.derivePath(_derrivationPathStorage).derive(++_derrivationChildStorage);
+    bip32.BIP32 keyComms = rootkey.derivePath(_derrivationPathCommunication).derive(_derrivationChildCommunication);
 
-    this._keys["wallet_privateKey"] = this._getPrivate(keyWallet);
-    this._keys["wallet_publicKey"] = this._privateToPublic(keyWallet);
-    this._keys["wallet_address"] = this._publicKeyToAddress(keyWallet);
+    _keys["wallet_privateKey"] = _getPrivate(keyWallet);
 
-    this._keys["storage_root_privateKey"] = this._getPrivate(keyStorage);
-    this._keys["storage_root_publicKey"] = this._privateToPublic(keyStorage);
+    _keys["wallet_publicKey"] = _privateToPublic(keyWallet);
+    _keys["wallet_address"] = _publicKeyToAddress(keyWallet);
 
-    this._keys["storage_vdb_privateKey"] = this._getPrivate(keyStorageVdb);
-    this._keys["storage_vdb_publicKey"] = this._privateToPublic(keyStorageVdb);
+    _keys["storage_root_privateKey"] = _getPrivate(keyStorage);
+    _keys["storage_root_publicKey"] = _privateToPublic(keyStorage);
 
-    this._keys["storage_vdc_privateKey"] = this._getPrivate(keyStorageVdc);
-    this._keys["storage_vdc_publicKey"] = this._privateToPublic(keyStorageVdc);
+    _keys["storage_vdb_privateKey"] = _getPrivate(keyStorageVdb);
+    _keys["storage_vdb_publicKey"] = _privateToPublic(keyStorageVdb);
+
+    _keys["storage_vdc_privateKey"] = _getPrivate(keyStorageVdc);
+    _keys["storage_vdc_publicKey"] = _privateToPublic(keyStorageVdc);
 
 
-    this._keys["comms_privateKey"] = this._getPrivate(keyComms);
-    this._keys["comms_publicKey"] =this._privateToPublic(keyComms);
+    _keys["comms_privateKey"] = _getPrivate(keyComms);
+    _keys["comms_publicKey"] = _privateToPublic(keyComms);
 
   }
 
-  String _getPrivate(dynamic _key) {
-    return bufferToHex(_key.privateKey);
+  String _getPrivate(dynamic key) {
+    return "0x${BytesUtils.toHexString(key.privateKey)}";
   }
 
-  String _privateToPublic(dynamic _key) {
-    return bufferToHex(privateKeyToPublicKey(_key.privateKey));
+  String _privateToPublic(dynamic key) {
+    return "0x${BytesUtils.toHexString(
+      ETHSigner.fromKeyBytes(key.privateKey)
+        .toVerifyKey().edsaVerifyKey.publicKey.toBytes(EncodeType.raw)
+    )}";
   }
 
-  String _publicKeyToAddress(dynamic _key) {
-    return bufferToHex(privateKeyToAddress(_key.privateKey));
+  String _publicKeyToAddress(dynamic key) {
+    return "0x${EthAddrEncoder().encodeKey(
+      BytesUtils.fromHexString(
+        _privateToPublic(key)
+      ),
+      {"skip_chksum_enc": true}
+    )}";
   }
   
   HashMap<String,String> get keys {
-    return this._keys;
+    return _keys;
   }
 
 }
 
 class KeyManagerUtils {
 
-  static String signPersonalMessage(String _message, String _privateKey) {
-    Uint8List msgHash = hashPersonalMessage(toBuffer(_message));
-    ECDSASignature signature = sign(msgHash, Uint8List.fromList(hex.decode(stripHexPrefix(_privateKey))));
-    return toRpcSig(signature.r, signature.s, signature.v);
+  static String signPersonalMessage(String message, String privateKey) {
+
+    final signer = ETHSigner.fromKeyBytes(BytesUtils.fromHexString(privateKey));
+    final encodedMessage = BytesUtils.fromHexString(
+      hex.encode(
+        StringUtils.encode(message, type: StringEncoding.ascii)
+      )
+    );
+    final sign = signer.signProsonalMessage(encodedMessage);
+    return "0x${BytesUtils.toHexString(sign)}";
   }
 
-  static String getPublicKeyFromSignature(String _message, String _signed) {
-    ECDSASignature signature = fromRpcSig(_signed);
-    Uint8List msgHash = hashPersonalMessage(toBuffer(_message));
-    Uint8List pubkey = recoverPublicKeyFromSignature(signature, msgHash);
-    return bufferToHex(publicKeyToAddress(pubkey));
+  static String getPublicKeyFromSignature(String message, String signed) {
+    
+    final encodedMessage = BytesUtils.fromHexString(
+      hex.encode(
+        StringUtils.encode(message, type: StringEncoding.ascii)
+      )
+    );
+
+    ECDSAPublicKey? publicKey = ETHVerifier.getPublicKey(encodedMessage, BytesUtils.fromHexString(signed));
+    final publicKeyBytes = publicKey?.toBytes();
+
+    List<int> realPubKey = List.empty();
+    if(publicKeyBytes != null) {
+      realPubKey = publicKeyBytes;
+    }
+    
+    return "0x${EthAddrEncoder().encodeKey(realPubKey, {"skip_chksum_enc": true})}";
   }
 }
--- test/keymanager_test.dart
+++ test/keymanager_test.dart
@@ -3,53 +3,52 @@ import 'package:keymanager/keymanager.dart';
 
 void main() async {
 
-//   String mnemonic = "exchange neither monster ethics bless cancel ghost excite business record warfare invite";
+  String mnemonic = "exchange neither monster ethics bless cancel ghost excite business record warfare invite";
   // String mnemonic = "skin opera hungry bundle relief meadow act unlock click only race whale";
-  // String ethereumWallet0privateKey = "0xe49c840fcb71fafcaa068c7d45a6b99f8d5b6064effe7d793b6490641e75cca8";
-  // String ethereumWallet0publicKey = "0x30bfa6298178e3ab1f4a2e5d5c3c7d79505c0b3ef7958ac0fec319a67d3e47eb01f05a7059311a6d061b7a4c1eff38549909b0782256e22f748cb9e6f3c4c4a4";
-  // String ethereumWallet0address = "0x9d9216e0a29468be1ecacc351ce3887be8a26222";
+  String ethereumWallet0privateKey = "0xe49c840fcb71fafcaa068c7d45a6b99f8d5b6064effe7d793b6490641e75cca8";
+  String ethereumWallet0publicKey = "0x30bfa6298178e3ab1f4a2e5d5c3c7d79505c0b3ef7958ac0fec319a67d3e47eb01f05a7059311a6d061b7a4c1eff38549909b0782256e22f748cb9e6f3c4c4a4";
+  String ethereumWallet0address = "0x9d9216e0a29468be1ecacc351ce3887be8a26222";
   // String signedMessage = "0x9cfd50e94fe93fd9d851085d119295e0b9c7a34ea5b8744fb359031c00ffcfd86455b222018c62607d8ea31d85d721ed73e34dd15cd4ca256c14438b870b9c7b1b";
-  
   // String mnemonic2 = "seat senior relief nation blade security wisdom coral place rely furnace umbrella";
   
-  // final myKeyManager = new KeyManager(mnemonic2);
-  // await myKeyManager.init();
+  final myKeyManager = KeyManager(mnemonic);
+  await myKeyManager.init();
 
-  // test('Generates correct wallet 0 private key', () {
-  //   expect(myKeyManager.keys["wallet_privateKey"], ethereumWallet0privateKey);
-  // });
+  test('Generates correct wallet 0 private key', () {
+    expect(myKeyManager.keys["wallet_privateKey"], ethereumWallet0privateKey);
+  });
 
-  // test('Generates correct wallet 0 public key', () {
-  //   expect(myKeyManager.keys["wallet_publicKey"], ethereumWallet0publicKey);
-  // });
+  test('Generates correct wallet 0 public key', () {
+    expect(myKeyManager.keys["wallet_publicKey"], ethereumWallet0publicKey);
+  });
 
-  // test('Generates correct wallet 0 address', () {
-  //   expect(myKeyManager.keys["wallet_address"], ethereumWallet0address);
-  // });
+  test('Generates correct wallet 0 address', () {
+    expect(myKeyManager.keys["wallet_address"], ethereumWallet0address);
+  });
 
-  // test('Generates all 6 keys', () {
-  //   expect(myKeyManager.keys["wallet_privateKey"].length, 66);
-  //   expect(myKeyManager.keys["storage_privateKey"].length, 66);
-  //   expect(myKeyManager.keys["comms_privateKey"].length, 66);
+  test('Generates all 6 keys', () {
+    expect(myKeyManager.keys["wallet_privateKey"]?.length, 66);
+    expect(myKeyManager.keys["storage_root_privateKey"]?.length, 66);
+    expect(myKeyManager.keys["comms_privateKey"]?.length, 66);
 
-  //   expect(myKeyManager.keys["wallet_publicKey"].length, 130);
-  //   expect(myKeyManager.keys["storage_publicKey"].length, 130);
-  //   expect(myKeyManager.keys["comms_publicKey"].length, 130);
-  // });
+    expect(myKeyManager.keys["wallet_publicKey"]?.length, 130);
+    expect(myKeyManager.keys["storage_root_publicKey"]?.length, 130);
+    expect(myKeyManager.keys["comms_publicKey"]?.length, 130);
+  });
 
   String key = "0x58c00e6af3e9ecca722c48bddbdfcb696f6939813dc1bad20cef5d852009055e";
   String signerAddress = "0x92faf690c46692df4203fce7263e20437d3dabc0";
 
   String msg = '{"data":{"data":"Register","date":"2021-01-27 21:47:40","e":251},"address":"0x92faf690c46692df4203fce7263e20437d3dabc0"}';
-  String signedMessage = "0x2c4672e19bd21e85d27b6c61dd37b009f2a191f3b7e943870c4d7a97d49164ae0035b510e1e86d9b6dbf53e2cd5817cce0d88833647ce33cf70279cdb0c7354d1c";
+  String signedMessage2 = "0x2c4672e19bd21e85d27b6c61dd37b009f2a191f3b7e943870c4d7a97d49164ae0035b510e1e86d9b6dbf53e2cd5817cce0d88833647ce33cf70279cdb0c7354d1c";
   test('signPersonalMessage returns correct string', () {
 
     String signed = KeyManagerUtils.signPersonalMessage(msg, key);
-    expect(signed, signedMessage);
+    expect(signed, signedMessage2);
   });
 
   test('getPublicKeyFromSignature returns correct address', () {
-    String address = KeyManagerUtils.getPublicKeyFromSignature(msg, signedMessage);
+    String address = KeyManagerUtils.getPublicKeyFromSignature(msg, signedMessage2);
     expect(address, signerAddress);
   });
 
@@ -67,14 +66,20 @@ void main() async {
     expect(address, signerAddress);
   });
 
-  // // debug
-  // print("wallet_privateKey:  " + myKeyManager.keys["wallet_privateKey"]);
-  // print("wallet_publicKey:   " + myKeyManager.keys["wallet_publicKey"]);
-  // print("wallet_address:     " + myKeyManager.keys["wallet_address"]);
-  // print("storage_privateKey: " + myKeyManager.keys["storage_privateKey"]);
-  // print("storage_publicKey:  " + myKeyManager.keys["storage_publicKey"]);
-  // print("comms_privateKey:   " + myKeyManager.keys["comms_privateKey"]);
-  // print("comms_publicKey:    " + myKeyManager.keys["comms_publicKey"]);
+  // debug
+  // print("wallet_privateKey:         ${myKeyManager.keys["wallet_privateKey"]}");
+  // print("wallet_publicKey:          ${myKeyManager.keys["wallet_publicKey"]}");
+  // print("wallet_address:            ${myKeyManager.keys["wallet_address"]}");
+  // print("storage_root_privateKey:   ${myKeyManager.keys["storage_root_privateKey"]}");
+  // print("storage_root_publicKey:    ${myKeyManager.keys["storage_root_publicKey"]}");
 
+  // print("storage_vdb_privateKey:    ${myKeyManager.keys["storage_vdb_privateKey"]}");
+  // print("storage_vdb_publicKey:     ${myKeyManager.keys["storage_vdb_publicKey"]}");
+
+  // print("storage_vdc_privateKey:    ${myKeyManager.keys["storage_vdc_privateKey"]}");
+  // print("storage_vdc_publicKey:     ${myKeyManager.keys["storage_vdc_publicKey"]}");
+
+  // print("comms_privateKey:          ${myKeyManager.keys["comms_privateKey"]}");
+  // print("comms_publicKey:           ${myKeyManager.keys["comms_publicKey"]}");
 
 }
